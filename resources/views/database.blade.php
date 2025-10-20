1fcf1b603048e8e36d0659b977569a6dcc7e8520
Calvinputra <calvinputranirwana2001@gmail.com>
2025-10-20 17:40:36 +0700

    test

--- resources/views/database.blade.php
+++ resources/views/database.blade.php
@@ -135,13 +135,11 @@
                                         <li class="font-semibold text-yellow-300">
                                             {{ $m->nickname }}
                                             <span class="ml-1 text-xs bg-yellow-500/20 text-yellow-300 px-2 py-0.5 rounded-full">Ketua</span>
-                                            <span class="text-blue-200/60 text-xs">({{ $m->email }})</span>
                                         </li>
                                     @endforeach
 
                                     @foreach ($anggota as $m)
                                         <li>{{ $m->nickname }}
-                                            <span class="text-blue-200/60 text-xs">({{ $m->email }})</span>
                                         </li>
                                     @endforeach
                                 </ul>
